28e772947c9ba86c5aa7b8bdef720122fdec4f14
abhishekbitlingu <abhishekbitlingu@gmailcom>
2017-10-30 20:33:09 +0530

    database setup

--- tailor-software/Login.cs
+++ tailor-software/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,24 @@ namespace tailor_software
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB; AttachDbFilename=C:\\Users\\abhis\\source\\repos\\tailor-software\\tailor-software\\db.mdf; Integrated Security=True; Connect Timeout=30";
+            con.Open();
+            string userId = userNameTextBox.Text;
+            string password = passwordTextBox.Text;
+            SqlCommand command = new SqlCommand("select UserName,Password from LoginTable where UserName='" + userId + "'and Password='" + password + "'", con);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            if (dataTable.Rows.Count > 0)
+            {
+                MessageBox.Show("Login sucess Welcome to Homepage ");
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
+            con.Close();
         }
 
         private void forgotPasswordLink_Click(object sender, EventArgs e)
